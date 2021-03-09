d5502d337aff00832b52fb4feb1678fdeb7973f0
Leonardo Maia <leuow95@gmail.com>
2021-03-08 22:54:00 -0300

    refactored lifecycle from ResultActivity to MainActivity

--- app/src/main/java/com/example/calculadoraimc_kotlin/ResultActivity.kt
+++ app/src/main/java/com/example/calculadoraimc_kotlin/ResultActivity.kt
@@ -10,30 +10,4 @@ class ResultActivity : AppCompatActivity() {
         setContentView(R.layout.activity_result)
         Log.w("lifecycle", "entrei no CREATE - estou criando a tela")
     }
-
-    override fun onStart() {
-        super.onStart()
-        Log.w("lifecycle", "entrei no START - deixei a tela visível")
-    }
-    override fun onResume() {
-        super.onResume()
-        Log.w("lifecycle", "entrei no RESUME - agora voce pode interagir com a tela")
-    }
-    override fun onPause() {
-        super.onPause()
-        Log.w("lifecycle", "entrei no PAUSE - a tela perdeu o foco, voce nao pode mais interagir")
-    }
-    override fun onStop() {
-        super.onStop()
-        Log.w("lifecycle", "entrei no STOP - a tela não está mais visível mas ainda existe")
-    }
-    override fun onRestart() {
-        super.onRestart()
-        Log.w("lifecycle", "entrei no RESTART - a tela esta retomando o foco")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.w("lifecycle", "entrei no DESTROY - a tela foi destruída")
-    }
-
 }
